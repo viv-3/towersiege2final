56b202b35bc97e1e57a476433b695e1abf88008f
viv-3 <72171270+viv-3@users.noreply.github.com>
2021-01-06 19:01:24 -0800

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,22 @@
+class Box extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.Visibility = 255;
+    }
+
+    display(){
+    
+    if(this.body.speed<3)
+    {
+      fill("purple")
+    super.display();
+  } else{
+    World.remove(world, this.body);
+    push();
+    this.Visibility = this.Visibility - 5;
+    tint(255,this.Visibility);
+    pop();
+  }
+      
+    }
+  };
